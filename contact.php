4db16573074312fa54f62d5481c86525ea58b1e0
Brennan Novak <hi@brennannovak.com>
2013-05-23 14:25:29 +0000

    Added contact sending via Mandrill and fixed some UI feedback copy

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <?php
+
 function clean_name($tag) {  
 	$code_ent_match = array(' ','-','--','---','&quot;','!','@','#','$','%','^','&','*','(',')','+','{','}','|',':','"','<','>','?','[',']','\\',';',"'",',','.','/','*','+','~','`','='); 
 	$code_ent_replace = array(' ','','','','','','','','','','','','','','','','','','','','','','','','','','',''); 
@@ -31,8 +32,8 @@ $LanguageMsgs = array(
 		'photo'		=> 'Vinsamlegast sláðu inn símanúmer.',
 		'email'		=> 'Þú verður að gefa upp netfang.',
 		'email_invalid' => 'Þetta netfang er ekki gilt.',
-		'success'	=> 'Skeytið var send!',
-		'error'		=> 'Við gátum ekki sent skilaboð.'
+		'success'	=> 'Skeytið var sent!',
+		'error'		=> 'Við gátum ekki sent skilaboðin.'
 	)
 );
 
@@ -40,53 +41,91 @@ $Language = stripslashes($_POST['language']);
 $ResultMessage = '';
 
 // Name
-if (isset($_POST['name'])) {
+if (isset($_POST['name'])):
 	$Name 			= stripslashes(clean_name($_POST['name']));
 	$NameState 		= true;
-}
-else {
+else:
 	$NameState 		= false;
 	$ResultMessage .= $LanguageMsgs[$Language]['name'];
-}	
+endif;
+
 
 // Phone
-if (isset($_POST['phone'])) {
+if (isset($_POST['phone'])):
 	$Phone			= stripslashes($_POST['phone']);
 	$PhoneState 	= true;
-}
-else {
+else:
 	$PhoneState 	= false;
 	$ResultMessage .= $LanguageMsgs[$Language]['phone'];
-}
+endif;
+
 
 // Email
-if (check_email_address($_POST['email'])) {
+if (check_email_address($_POST['email'])):
 	$Email 			= stripslashes($_POST['email']);
 	$EmailState 	= true;
-}
-else {
+else:
 	$EmailState 	=  false;
 	$ResultMessage .= $LanguageMsgs[$Language]['email_invalid'];
-}
+endif;
+
 
 
 
 //CHECKS TO SEE IF ALL THREE REQUIRED FIELDS ARE FILLED IN	
-if (($NameState == true) && ($EmailState == true) && ($PhoneState == true)) {
+if (($NameState == true) && ($EmailState == true) && ($PhoneState == true)):
 
 	//SEND MEMBER EMAIL
-	$EmailTo   		= "Vísar <visar@visar.is>";
-	$EmailFrom     	= $Name." <".$Email.">";
-	$Subject     	= "Contact message from Visar.is";
-	$Headers 	    = 'From: '.$EmailFrom."\r\n";
-	$Message		= $Name.'<br>'.$Email.'<br>'.$Phone.'<br>'.$_POST['comment'];
-	$SentMessage 	= mail($EmailTo, $Subject, $Message, $Headers);
+	$MessagePost = '{
+		"key": "VtTYH7yjVEoIVwCpIahAyg",
+		"message": {
+		    "html": "<p>Name: '.$Name.'<br>Email: '.$Email.'<br>Phone: '.$Phone.'</p><p>'.$_POST['comment'].'</p>",
+		    "text": "Name: '.$Name.' - Email: '.$Email.' - Phone: '.$Phone.' - '.$_POST['comment'].'",
+		    "subject": "Contact from Visar.is",
+		    "from_email": "'.$Email.'",
+		    "from_name": "'.$Name.'",
+		    "to": [{
+		    	"email": "visar@visar.is",
+		    	"name": "Vísar"	
+		    }],
+		    "headers": {},
+		    "track_opens": true,
+		    "track_clicks": true,
+		    "auto_text": true,
+		    "url_strip_qs": true,
+		    "preserve_recipients": true,
+		    "merge": true,
+		    "global_merge_vars": [],
+		    "merge_vars": [],
+		    "tags": [],
+		    "google_analytics_domains": [],
+		    "google_analytics_campaign": "...",
+		    "metadata": [],
+		    "recipient_metadata": [],
+		    "attachments": []
+		},
+		"async": false
+	}';
 
-	echo json_encode(array('status' => 'success', 'message' => $LanguageMsgs[$Language]['success']));
-}
-else
-{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://mandrillapp.com/api/1.0/messages/send.json');
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $MessagePost);
+
+	$json_result = curl_exec($ch);
+	$result = json_decode($json_result);
+
+	if ($result[0]->status == 'sent'):
+		echo json_encode(array('status' => 'success', 'message' => $LanguageMsgs[$Language]['success']));
+	else: 
+		echo json_encode(array('error' => 'success', 'message' => $LanguageMsgs[$Language]['error']));		
+	endif;
+
+else:
 	$ErrorMessage = $LanguageMsgs[$Language]['error'].' '.$ResultMessage;
 
 	echo json_encode(array('status' => 'error', 'message' => $ErrorMessage));
-}
+endif;
